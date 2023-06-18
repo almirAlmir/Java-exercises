1de1ba6699e2831446993ba4180c3873ccbad01d
almirAlmir <aasc@cin.ufpe.br>
2023-06-18 15:33:32 -0300

    4042 almost complete but don''t work with '0x100' case

--- theHuxley/Problem4042.java
+++ theHuxley/Problem4042.java
@@ -7,9 +7,12 @@ import java.util.Scanner;
 
 
 //Program by: Almir Alves de Souza Cruz
-//24/05/2023
+//18/05/2023
 //LEMBRE DE COLOCAR A DATA CERTA QUANDO ACABAR
 public class Problem4042 {
+	
+final static int [] horizontal = {-1, 1, 0, 0};	
+final static int [] vertical = {0, 0, -1, 1};
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -19,7 +22,7 @@ public class Problem4042 {
 		int line = Integer.parseInt(ml.substring(0, 1));
 		int column = Integer.parseInt(ml.substring(2));
 		char [] [] labyrinth = new char [line] [column];
-		
+		Coord [] origemDestino = new Coord[2];
 		
 		for(int i = 0; i < line; i++) {
 			
@@ -30,15 +33,30 @@ public class Problem4042 {
 				
 				labyrinth [i] [j] = str.charAt(j);
 				
+				if(labyrinth [i][j] == 'o') {
+		 
+					origemDestino[0] = new Coord(i, j);
+				}else if(labyrinth [i][j] == 'd') {
+					
+					origemDestino[1] = new Coord(i, j);
+				}
+				
 			}
 		}
-	
+			
+		int result = BFS(origemDestino[0], origemDestino[1], labyrinth);
+		
+		if(result == -1) {
+			System.out.println("Poxa... Parece que nao foi dessa vez que Rebeka conseguiu fugir");
+		}else {
+			System.out.println("Apos correr "+result+" metros e quase desistir por causa da distância, Rebeka conseguiu escapar!");
+		}
 		
 	}
 	
-	public int BFS(Coord o, Coord d, char [][] lab) {
+	public static int BFS(Coord o, Coord d, char [][] lab) {
 		
-		BlockingQueue<Coord> coordQueue = new LinkedBlockingQueue<>();
+		BlockingQueue<Coord> coordQueue = new LinkedBlockingQueue<>(); //frontier
 		coordQueue.add(o);
 		
 		int [][] visited = new int [lab.length] [lab[0].length];
@@ -51,8 +69,26 @@ public class Problem4042 {
 				return dist[checkingNow.x][checkingNow.y];
 			
 			//Here I need to implement the <- ^ -> v moves(left, up, right, down)
+			for(int i = 0; i < 4; i++) {
+				
+				int nextX = checkingNow.x + horizontal[i];
+				int nextY = checkingNow.y + vertical[i];
+				
+				boolean insideMatrix = false;
+				
+				if(nextX >= 0 && nextY >= 0 && nextX < lab.length && nextY < lab[0].length) {
+					insideMatrix = true;
+				}
+				
+				if(insideMatrix && lab[nextX][nextY] != '#' && visited[nextX][nextY] == 0) {
+					
+					dist[nextX][nextY] = 1 + dist[checkingNow.x][checkingNow.y];
+					visited[nextX][nextY] = 1;
+					coordQueue.add(new Coord(nextX, nextY));
+				}
+				
+			}
 			
-			//each cell I visit I'll mark visited and put in the Queue
 			
 			//Do this 'til found D or end the matrix
 			
@@ -61,7 +97,7 @@ public class Problem4042 {
 		return -1;
 	}
 	
-	public class Coord{
+	public static class Coord{
 		//representa o ponto X e Y da matriz onde o elemento se encontra
 		private int x;
 		private int y;
@@ -71,6 +107,7 @@ public class Problem4042 {
 			this.x = x;
 			this.y = y;
 		}
+		
 		
 	}
 	
